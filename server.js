b8c9dabcf36ab0f0a193b8088f099fd4cb4ed7e0
eopit <pedro.barricelli@gmail.com>
2020-02-23 23:46:13 -0300

    final front

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.static('public'));
+
+const nunjucks = require('nunjucks');
+nunjucks.configure('./', {
+  express: server
+});
+
+server.get('/', function(req, res) {
+  return res.render('index.html');
+});
+
+server.listen(3000, function() {
+  console.log('Listening on 3000');
+});
